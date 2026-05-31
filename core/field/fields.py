26b1d0fec54a5fd4d89f4d2acfea70fd04c2a227
Konstantinos Priftis <priftis.kostas1997@gmail.com>
2026-05-31 17:20:09 +0300

    Charfield added

--- core/field/fields.py
+++ core/field/fields.py
@@ -5,3 +5,16 @@ class IntField(Field):
     def validate(self, value):
         if not isinstance(value, int):
             raise TypeError("must be int")
+
+
+class CharField(Field):
+    def __init__(self, default=None, max_length=255):
+        self.max_length = max_length
+        super().__init__(default)
+
+    def validate(self, value):
+        if len(value) > self.max_length:
+            raise ValueError(
+                f"Value too long for Charfield(max_length={self.max_length})"
+            )
+        return super().validate(value)
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from core.model.base import Model
-from core.field.fields import IntField
+from core.field.fields import IntField, CharField
 
 
 class User(Model):
     id = IntField(default=1)
     age = IntField(default=20)
+    name = CharField(max_length=10, default="sbuiad")
 
 
-u = User(id="abc", age="x")
+u = User()
 
 print(u.errors)
-print(u.is_valid)
+print(u)
